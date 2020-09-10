aaf1a565e17774934bc460dd596ef32f0ce40066
bogdanmatei <matei90bogdan@yahoo.com>
2020-09-10 20:53:06 +0300

    Created components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
