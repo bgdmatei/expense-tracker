dd07a601fe7444fdf7046fa2a561a98f322252f4
bogdanmatei <matei90bogdan@yahoo.com>
2020-09-20 15:38:25 +0300

    Adding transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { GlobalContext } from "../context/GlobalState";
 
 export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
@@ -8,7 +11,12 @@ export const Transaction = ({ transaction }) => {
       <span>
         {sign}£{Math.abs(transaction.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 };
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,14 +3,7 @@ import AppReducer from "./AppReducer";
 
 // Initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: "Cars", amount: -3000 },
-    {
-      id: 2,
-      text: "Salary",
-      amount: 1500,
-    },
-  ],
+  transactions: [],
 };
 
 // Create context
@@ -21,8 +14,29 @@ export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
