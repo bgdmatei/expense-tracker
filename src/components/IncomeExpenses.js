00f09bf7902ab6aaa36b169d330a3f03c01d56f7
bogdanmatei <matei90bogdan@yahoo.com>
2020-09-20 14:36:40 +0300

    Displaying transaction list and balance

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
   return (
     <div className="inc-exp-container">
       <div>
