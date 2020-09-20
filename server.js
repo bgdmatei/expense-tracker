f39fe2c928da971a83b62c70beac5108428e8b6c
bogdanmatei <matei90bogdan@yahoo.com>
2020-09-20 18:53:40 +0300

    Initialized express

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+);
